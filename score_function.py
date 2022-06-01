52e71a872359de8fab2012a7fd46bd1cf4728bc7
pauldd <paul.dobner98@gmail.com>
2022-06-01 20:25:04 +0200

    add score function

---
+++ score_function.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn.metrics import make_scorer
+
+
+def my_score_function(y_true, y_pred):
+    """
+    Return the score according to the Estimated revenues/ fitmotion gain matrix
+    from the slides.
+    labels: 1="happy", 2="angry", 3="sad", 4="relaxed"
+    for sklearn use this:
+        from sklearn.metrics import make_scorer
+        score = make_scorer(my_score_function, greater_is_better=True)
+        score(classifier, X, y) # same as my_score_function(y, classifier.predict(X))
+
+    :param y_true: true labels as numpy array of int
+    :param y_pred: predicted labels as numpy array of int
+    :return: accumulates score over all predictions
+    """
+    # convert to int and remove 1 to use as index for gain_matrix
+    y_true = y_true.astype(int) - 1
+    y_pred = y_pred.astype(int) - 1
+
+    x = -1e10  # padding to make indexing possible
+    gain_matrix = np.array([[5, -5, -5, 2],
+                            [-5, 10, 2, -5],
+                            [-5, 2, 10, -5],
+                            [2, -5, -2, 5]])  # actual and predicted = "happy": revenue_matrix[1,1] => 5
+
+    score = gain_matrix[y_true, y_pred].sum()
+
+    return score
