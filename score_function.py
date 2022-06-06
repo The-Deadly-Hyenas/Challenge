2e595a15fe9438164b0845efc10442cb5587a9bd
pauldd <paul.dobner98@gmail.com>
2022-06-06 22:22:35 +0200

    update/ add revenue function

--- score_function.py
+++ score_function.py
@@ -1,16 +1,15 @@
 import numpy as np
-from sklearn.metrics import make_scorer
 
 
-def my_score_function(y_true, y_pred):
+def revenue_gain(y_true, y_pred):
     """
     Return the score according to the Estimated revenues/ fitmotion gain matrix
-    from the slides.
+    from the slides. Faster than the revenue_challenge.py function.
     labels: 1="happy", 2="angry", 3="sad", 4="relaxed"
     for sklearn use this:
         from sklearn.metrics import make_scorer
-        score = make_scorer(my_score_function, greater_is_better=True)
-        score(classifier, X, y) # same as my_score_function(y, classifier.predict(X))
+        score = make_scorer(revenue_gain, greater_is_better=True)
+        score(classifier, X, y) # same as revenue_gain(y, classifier.predict(X))
 
     :param y_true: true labels as numpy array of int
     :param y_pred: predicted labels as numpy array of int
@@ -20,12 +19,11 @@ def my_score_function(y_true, y_pred):
     y_true = y_true.astype(int) - 1
     y_pred = y_pred.astype(int) - 1
 
-    x = -1e10  # padding to make indexing possible
     gain_matrix = np.array([[5, -5, -5, 2],
                             [-5, 10, 2, -5],
                             [-5, 2, 10, -5],
-                            [2, -5, -2, 5]])  # actual and predicted = "happy": revenue_matrix[1,1] => 5
+                            [2, -5, -2, 5]])
 
-    score = gain_matrix[y_true, y_pred].sum()
+    revenue = gain_matrix[y_true, y_pred].sum()
 
-    return score
+    return revenue
