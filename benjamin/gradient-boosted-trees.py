14aea753ff39b41f46ea0fd9e9e440465f6dd8a8
Benjamin Buzek <contact@benjaminbuzek.com>
2022-06-08 01:16:02 +0200

    added gbdt

---
+++ benjamin/gradient-boosted-trees.py
@@ -0,0 +1,146 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from simple_colors import *
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import SelectKBest, f_classif
+from imblearn.combine import SMOTEENN
+from sklearn.ensemble import GradientBoostingClassifier
+
+df: pd.DataFrame = pd.read_pickle(
+    "../training_data/task_3_training_e8da4715deef7d56_f8b7378_pandas.pkl").reset_index()
+
+
+class GBDT:
+
+    def __init__(self, params):
+        """
+        Default Parameters
+        :param params:
+        """
+        self.params = params
+
+        self.X = None
+        self.y = None
+
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
+
+        self.model = None
+        self.cv = None
+
+    def preprocessing(self):
+        """
+        Performe Dataset Pre-Processing and Cross-Validation
+        :return:
+        """
+        # Load Training Dataset (Low-Level Features and Mid-Level Features)
+        X = df.loc[:, "essentia_dissonance_mean":"mirtoolbox_roughness_pct_90"]
+        y = df["quadrant"]
+
+        # Dataset Pre-Processing
+        X_std = StandardScaler().fit_transform(X)
+        X = pd.DataFrame(X_std, columns=X.columns)
+
+        # add segment_id to training data for doing the cross validation splits
+        X["segment_id"] = df["segment_id"]
+
+        # remove segment_id 26
+        seg_26_indices = (X["segment_id"] == 26)
+        X_test = X[seg_26_indices].drop(["segment_id"], axis=1)
+        y_test = y[seg_26_indices]
+
+        X_train = X.drop(X[seg_26_indices].index, axis=0).reset_index(drop=True)
+        y_train = y.drop(X[seg_26_indices].index, axis=0)
+
+        # SMOTEENN
+        # https://imbalanced-learn.org/stable/combine.html
+        smote_enn = SMOTEENN(random_state=0)
+        X_resampled, y_resampled = smote_enn.fit_resample(X_train, y_train)
+
+        # CROSS-VALIDATION
+        cv = []
+        for i in tqdm(range(24), total=len(range(24)), desc=":: Cross-Validation"):
+            train_indices = X_resampled[~X_resampled["segment_id"].isin([i, i + 1])].index.to_list()
+            test_indices = X_resampled[X_resampled["segment_id"].isin([i, i + 1])].index.to_list()
+            cv.append((train_indices, test_indices))
+
+        # Drop Segment-ID
+        X_resampled = X_resampled.drop(["segment_id"], axis=1)
+
+        # Select Best Features
+        best_features = SelectKBest(score_func=f_classif, k=17).fit(X_resampled, y_resampled).get_feature_names_out()
+
+        self.cv = cv
+
+        self.X = X
+        self.y = y
+
+        self.X_train = X_resampled[best_features]
+        self.y_train = y_resampled
+
+        self.X_test = X_test[best_features]
+        self.y_test = y_test
+
+    def train_model(self):
+        """
+        Select model from model class
+        :return:
+        """
+
+        print(green(":: Starting Preprocessing"))
+        # Dataset Preprocessing
+        self.preprocessing()
+        print(green(":: Finished Preprocessing"))
+
+        gridSearchModel = GridSearchCV(GradientBoostingClassifier(), self.params, cv=self.cv, n_jobs=-1)
+        gridSearchModel.fit(self.X_train, self.y_train)
+
+        # Set Model
+        self.model = gridSearchModel
+
+        print(blue(f"\n:: Trainingset Score\t\t\t\t\t{self.model.score(self.X_train, self.y_train)}"))
+        print(blue(f":: Testset Score (Segment-ID: 26)\t\t{self.model.score(self.X_test, self.y_test)}"))
+
+    def test_model(self):
+        """
+        Train model on best model classifiers and test on test dataset
+        :return:
+        """
+        # load test dataset and transform
+        test_data = pd.read_pickle("../test_data/task_4_test_dd4bd32b08b776e6_daf99ad_pandas.pkl").reset_index()
+        test_features = test_data.loc[:, "essentia_dissonance_mean":]
+        test_features_std = StandardScaler().fit_transform(test_features)
+        test_features = pd.DataFrame(test_features_std, columns=test_features.columns)
+
+        # apply data transform to all the data
+        smote_enn = SMOTEENN(random_state=0)
+        X_all, y_all = smote_enn.fit_resample(self.X, self.y)
+        best_features = SelectKBest(score_func=f_classif, k=15).fit(X_all, y_all).get_feature_names_out()
+        X_all = X_all.drop(["segment_id"], axis=1)
+        X_all = X_all[best_features]
+
+        # predict labels
+        gbdt = GradientBoostingClassifier(**self.model.best_params_).fit(X_all, y_all)
+        predicted = gbdt.predict(test_features[best_features])
+
+        # create dataframe with results and save to csv
+        results = test_data.loc[:, "pianist_id":"snippet_id"]
+        results["quadrant"] = predicted.astype(int)
+        results.to_csv("results_gbdt.csv", index=False)
+
+
+if __name__ == "__main__":
+
+    params = {"n_estimators": [115],
+              "learning_rate": [0.01],
+              "min_samples_leaf": [130],
+              "max_depth": [15]}
+
+    GBDT = GBDT(params=params)
+    GBDT.train_model()
+    # GBDT.test_model()
